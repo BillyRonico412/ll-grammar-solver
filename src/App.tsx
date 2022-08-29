6d37c96c49a175beadad2a94894a18c3c189a4cc
BillyRonico <billyronico412@gmail.com>
2022-08-29 10:55:26 +0200

    add credits

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import { useSelector } from "react-redux";
-import {
-    Grammar,
-    Lexem,
-    Production,
-    LLInterface,
-} from "@ronico.billy/ll/lib/interface";
-import { LL } from "@ronico.billy/ll";
-import Button from "./components/Button";
 import { RootState } from "./store";
-import { notyf, isLowerCase, isUpperCase } from "./utils";
 import Result from "./components/Result";
 import { useDispatch } from "react-redux";
-import { appActions } from "./slices/app";
 import Add from "./components/Add";
 import { Route, Routes } from "react-router-dom";
+import { FaGithub, FaUserAlt } from "react-icons/fa";
 
 const App = () => {
     const dispatch = useDispatch();
     const prods = useSelector((state: RootState) => state.app.prods);
     return (
-        <div className="px-4 py-4 container mx-auto">
+        <div className="px-4 py-4 container mx-auto min-h-screen flex flex-col">
             <h1 className="font-black text-2xl text-center">
                 Grammar LL Solver
             </h1>
@@ -28,6 +19,23 @@ const App = () => {
                     <Route path="/" element={<Add />} />
                     <Route path="result" element={<Result />} />
                 </Routes>
+            </div>
+            <div className="pt-16 pb-4 mt-auto text-gray-600 text-center">
+                <p>Created by @RonicoBilly</p>
+                <div className="flex justify-center mt-3 gap-x-8">
+                    <a
+                        href="https://github.com/BillyRonico412/ll-grammar-solver"
+                        target={"_blank"}
+                    >
+                        <FaGithub />
+                    </a>
+                    <a
+                        href="https://billyronico412.github.io/"
+                        target={"_blank"}
+                    >
+                        <FaUserAlt />
+                    </a>
+                </div>
             </div>
         </div>
     );
